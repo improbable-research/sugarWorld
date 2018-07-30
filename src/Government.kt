a0f452f4270e3fdf66ecfa84a72d519c92486080
dmculley <daveculley@improbable.io>
2018-07-30 17:35:00 +0100

    First pass at government

--- src/Government.kt
+++ src/Government.kt
@@ -1,3 +1,44 @@
-class Government(country : Country) : Consumer() {
-    fun step() {}
+import java.lang.Math.exp
+
+class Government(var country : Country) : Consumer() {
+
+    var perCapitaGDP = 0.0
+    var previousPerCapitaGDP = 0.0
+    var desiredPerCapitaGDP = 1.0
+
+    fun step () {
+        // Compute change in GDP
+        perCapitaGDP = calculateGDPperCapita()
+        val changeInGDP = perCapitaGDP - previousPerCapitaGDP
+        previousPerCapitaGDP = perCapitaGDP
+
+        // Decide the extent to which to stimulate the economy through spending
+        val amountToBuy = calculateAmountToBuy(changeInGDP)
+        country.industry.sellSugar(amountToBuy)
+    }
+
+    fun calculateTaxPercentage (grossIncome: Double) : Double {
+        var minTax = 0.1
+        var maxTax = 0.6
+        var topRateThreshold = 2.0
+        var slopeOfTransition = 1.0
+        var transitionLocation = 0.5
+        return topRateThreshold *
+                 (minTax * exp(transitionLocation * slopeOfTransition) + maxTax * exp(slopeOfTransition * grossIncome)) /
+                 (Math.exp(transitionLocation*slopeOfTransition) + exp(slopeOfTransition * grossIncome))
+    }
+
+    fun calculateAmountToBuy (changeInGDP: Double) : Double {
+        var noTimestepsAheadToForecast = 1
+        var expectedGDPperCapita = perCapitaGDP + (noTimestepsAheadToForecast * changeInGDP)
+        return (desiredPerCapitaGDP - expectedGDPperCapita) * country.population
+    }
+
+
+    fun calculateGDPperCapita () : Double {
+        var priceOfSugar = 1.0
+        return (country.industry.sales * priceOfSugar) / country.population
+    }
+
+
 }
